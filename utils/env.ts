e0bf7ca29227027bc9dce0a9c2b4bb8f16b734f8
Priyanka <priyankasundarum@gmail.com>
2026-08-14 08:49:08 +0530

    Fix CI environment variable configuration

--- utils/env.ts
+++ utils/env.ts
@@ -1,5 +1,17 @@
 export const env = {
-  baseUrl: process.env.BASE_URL!,
-  username: process.env.HRM_USERNAME!,
-  password: process.env.HRM_PASSWORD!
+  baseUrl: process.env.BASE_URL,
+  username: process.env.HRM_USERNAME,
+  password: process.env.HRM_PASSWORD,
 };
+
+if (!env.baseUrl) {
+  throw new Error("BASE_URL environment variable is missing");
+}
+
+if (!env.username) {
+  throw new Error("USERNAME environment variable is missing");
+}
+
+if (!env.password) {
+  throw new Error("PASSWORD environment variable is missing");
+}
